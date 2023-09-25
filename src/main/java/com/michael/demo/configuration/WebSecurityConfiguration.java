8f97b90a214dbb3df6a87dd3d001719b09f262b5
Michael Olowoyo <Michael.Olowoyo@Interswitchng.com>
2023-09-25 16:49:04 +0100

    added config

---
+++ src/main/java/com/michael/demo/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.michael.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+}
